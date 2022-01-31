29179ce4190710895172f306f5cab5882b417ef6
Jokra005 <jokra005@osloskolen.no>
2022-01-31 15:03:14 +0100

    Initial commit

---
+++ avvik.md
@@ -0,0 +1,6 @@
+[js/main.js:6](js/main.js) class er med liten C.  
+[js/main.js:14](js/main.js) trenger ikke function keyword i klasse.
+[js/main.js:15](js/main.js) .fillRect, ikke .drawRect
+[js/main.js:18](js/main.js) trenger ikke function keyword i klasse.
+[js/main.js:31](js/main.js) inverted y-akse.
+[js/main.js:34](js/main.js) inverted y-akse.
---
+++ js/main.js
@@ -0,0 +1,46 @@
+const canvasEl = document.getElementById('canvas');
+const ctx = canvasEl.getContext('2d');
+
+const statusEl = document.getElementById('status');
+
+class Square {
+	constructor(x,y,w,h){
+		this._x = x;
+		this._y = y;
+		this._w = w;
+		this._h = h;
+	}
+
+	draw(ctx){
+		ctx.fillRect(this._x, this._y, this._w, this._h);
+	}
+
+	move(dx,dy){
+		this._x += dx;
+		this._y += dy;
+	}
+}
+
+const square = new Square(50,50,50,50);
+
+square.draw(ctx);
+
+document.addEventListener('keydown', e=>{
+	switch(e.key){
+		case 'ArrowUp':
+			square.move(0,-10);
+			break;
+		case 'ArrowDown':
+			square.move(0,10);
+			break;
+		case 'ArrowRight':
+			square.move(10,0);
+			break;
+		case 'ArrowLeft':
+			square.move(-10,0);
+			break;
+	}
+	ctx.clearRect(0,0,500,500);
+	square.draw(ctx);
+	if(square._x > 300) statusEl.innerHTML = 'Du vant';
+})
